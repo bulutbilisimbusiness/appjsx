ac224d2b4285689b006fff6f6f6f3459654a0417
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-02-29 23:18:06 +0300

    profile

--- ultimate/merndashboard/client/src/pages/Dashboard.jsx
+++ ultimate/merndashboard/client/src/pages/Dashboard.jsx
@@ -1,5 +1,22 @@
-function Dashboard() {
-	return <div>Dashboard</div>;
-}
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+export default function Dashboard() {
+	const location = useLocation();
+	const [tab, setTab] = useState("");
 
-export default Dashboard;
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const tabFromUrl = urlParams.get("tab");
+		if (tabFromUrl) setTab(tabFromUrl);
+	}, [location.search]);
+	return (
+		<div className="min-h-screen flex flex-col md:flex-row">
+			<div className="md:w-56">
+				<DashSidebar />
+			</div>
+			{tab === "profile" && <DashProfile />}
+		</div>
+	);
+}
