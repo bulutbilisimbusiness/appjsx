852c73f7508f2e56bbfb49b9cd425609ac43a6ab
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-02-29 09:08:59 +0300

    redux

--- ultimate/merndashboard/client/src/pages/SignIn.jsx
+++ ultimate/merndashboard/client/src/pages/SignIn.jsx
@@ -1,11 +1,16 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice";
 function SignIn() {
 	const [formData, setFormData] = useState({});
-	const [errorMessage, setErrorMessage] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const { loading, error: errorMessage } = useSelector((state) => state.user);
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -13,31 +18,27 @@ function SignIn() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		if (!formData.email || !formData.password) {
-			return setErrorMessage("Please fill out all fields");
+			return dispatch(signInFailure("Please fill out all fields"));
 		}
 		try {
-			setLoading(true);
-			setErrorMessage(null);
+			dispatch(signInStart());
 			const res = await fetch("/api/auth/signin", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" }, // Düzeltildi
 				body: JSON.stringify(formData),
 			});
-			if (!res.ok) {
-				// HTTP durum kodunu kontrol edin
-				throw new Error(`HTTP error! status: ${res.status}`);
-			}
+
 			const data = await res.json();
 			if (data.success === false) {
-				return setErrorMessage(data.message);
+				dispatch(signInFailure(data.message));
 			}
-			setLoading(false);
+
 			if (res.ok) {
+				dispatch(signInSuccess(data));
 				navigate("/");
 			}
 		} catch (error) {
-			setErrorMessage(error.message);
-			setLoading(false);
+			dispatch(signInFailure(error.message));
 		}
 	};
 
