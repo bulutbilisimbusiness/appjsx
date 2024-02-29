7304590f11d87ad99002caf738b26d58a3fe1d2d
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-02-29 11:25:58 +0300

    google

--- ultimate/merndashboard/client/src/pages/SignIn.jsx
+++ ultimate/merndashboard/client/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import {
 	signInSuccess,
 	signInFailure,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 function SignIn() {
 	const [formData, setFormData] = useState({});
 	const { loading, error: errorMessage } = useSelector((state) => state.user);
@@ -91,6 +92,7 @@ function SignIn() {
 								"Sign In"
 							)}
 						</Button>
+						<OAuth />
 					</form>
 					<div className="flex gap-2 text-sm mt-5">
 						<span>Dont have an account?</span>
