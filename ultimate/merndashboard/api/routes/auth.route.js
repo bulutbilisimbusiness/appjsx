fe88216415f3659ccbfca4df795a4c780d85e670
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-05 22:53:41 +0300

    post

--- ultimate/merndashboard/api/routes/auth.route.js
+++ ultimate/merndashboard/api/routes/auth.route.js
@@ -1,7 +1,7 @@
-import exoress from "express";
+import express from "express";
 import { signin, signup, google } from "../controllers/auth.controller.js";
 
-const router = exoress.Router();
+const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
