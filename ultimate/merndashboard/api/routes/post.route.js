a51acfd445a6c54ca2b31b58a1ad16c41c608b5a
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-07 14:17:32 +0300

    edit

--- ultimate/merndashboard/api/routes/post.route.js
+++ ultimate/merndashboard/api/routes/post.route.js
@@ -4,9 +4,11 @@ import {
 	create,
 	deletepost,
 	getposts,
+	updatepost,
 } from "../controllers/post.controller.js";
 const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 export default router;
