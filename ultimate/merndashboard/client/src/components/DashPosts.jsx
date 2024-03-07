3256e5774cada1e54f62718be0ecc0fb34714ac3
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-07 08:47:58 +0300

    showmore

--- ultimate/merndashboard/client/src/components/DashPosts.jsx
+++ ultimate/merndashboard/client/src/components/DashPosts.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
 	const { currentUser } = useSelector((state) => state.user);
 	const [userPosts, setUserPosts] = useState([]);
+	const [showMore, setShowMore] = useState(true);
 	useEffect(() => {
 		const fetchPosts = async () => {
 			try {
@@ -12,6 +13,9 @@ export default function DashPosts() {
 				const data = await res.json();
 				if (res.ok) {
 					setUserPosts(data.posts);
+					if (data.posts.length < 9) {
+						setShowMore(false);
+					}
 				}
 			} catch (error) {
 				console.log(error);
@@ -21,6 +25,24 @@ export default function DashPosts() {
 			fetchPosts();
 		}
 	}, [currentUser._id]);
+	const handleShowMore = async () => {
+		const startIndex = userPosts.length;
+
+		try {
+			const res = await fetch(
+				`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+			);
+			const data = await res.json();
+			if (res.ok) {
+				setUserPosts((prev) => [...prev, ...data.posts]);
+				if (data.posts.length < 9) {
+					setShowMore(false);
+				}
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 	return (
 		<div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 scrollbar-thumb-slate-500 ">
 			{currentUser.isAdmin && userPosts.length > 0 ? (
@@ -77,6 +99,14 @@ export default function DashPosts() {
 							</Table.Body>
 						))}
 					</Table>
+					{showMore && (
+						<button
+							onClick={handleShowMore}
+							className="w-full text-teal-500 self-center text-sm py-7"
+						>
+							Show more
+						</button>
+					)}
 				</>
 			) : (
 				<p>You have no posts yet!</p>
