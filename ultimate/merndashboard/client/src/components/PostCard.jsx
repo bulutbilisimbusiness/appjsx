062c7ebbdb5b6e82ae950493ad698d79c91b9bb6
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-12 11:27:52 +0300

    recent

--- ultimate/merndashboard/client/src/components/PostCard.jsx
+++ ultimate/merndashboard/client/src/components/PostCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function PostCard({ post }) {
 	return (
-		<div className="group relative w-full border h-[400px] overflow-hidden rounded-lg sm:w-[430px]">
+		<div className="group relative w-full border border-teal-500 hover:border-2 transition-all h-[400px] overflow-hidden rounded-lg sm:w-[430px]">
 			<Link to={`/post/${post.slug}`}>
 				<img
 					src={post.image}
