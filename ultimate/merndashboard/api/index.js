20d9f48fbf48e207c679234ad00c7be875352374
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-14 11:16:59 +0300

    fin

--- ultimate/merndashboard/api/index.js
+++ ultimate/merndashboard/api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -18,6 +19,7 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
+const __dirname = path.resolve();
 app.listen(3000, () => {
 	console.log("Server is  running on port 3000!");
 });
@@ -26,6 +28,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Internal Server Error";
