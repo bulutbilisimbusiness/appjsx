793742ed1c263a2b2137e868958fac9472d70906
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-17 14:37:26 +0300

    layout

--- cway/muivite/src/components/Typo.jsx
+++ cway/muivite/src/components/Typo.jsx
@@ -1,9 +1,30 @@
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 const Typo = () => {
 	return (
-		<div>
-			<Typography>TYPO</Typography>
-		</div>
+		<Box
+			sx={{
+				padding: "1rem",
+				display: "flex",
+				flexDirection: "column",
+				gap: "1rem",
+				border: "1px  solid tomato",
+			}}
+		>
+			<Typography variant="h6" component="h1" color="red" mt={4}>
+				TYPO
+			</Typography>
+			<Typography variant="button" color="palegreen">
+				typo
+			</Typography>
+			<Typography
+				variant="body2"
+				sx={{ color: "blue", backgroundColor: "palevioletred", mt: "1rem" }}
+				align="center"
+			>
+				button text
+			</Typography>
+		</Box>
 	);
 };
 
