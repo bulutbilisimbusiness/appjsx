c54b1c09374bad4c765e1112455d154d0207997b
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-18 23:54:32 +0300

    count

---
+++ cway/redux/src/components/counter/Counter.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import "./Counter.css";
+
+const Counter = () => {
+	const count = useSelector((state) => state.count);
+	const dispatch = useDispatch();
+	return (
+		<div className="app">
+			<h2 className="counter-header">Counter With Redux</h2>
+			<h1>counter: {count}</h1>
+			<div>
+				<button
+					className="counter-button positive"
+					onClick={() => dispatch({ type: "INC" })}
+				>
+					increase
+				</button>
+				<button
+					className="counter-button zero"
+					onClick={() => dispatch({ type: "CLR" })}
+				>
+					reset
+				</button>
+				<button
+					className="counter-button negative"
+					onClick={() => dispatch({ type: "DEC" })}
+				>
+					decrease
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
