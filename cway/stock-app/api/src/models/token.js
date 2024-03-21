0e282ed68e06e4de45a02b910f7439389a9e5fae
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-03-22 00:40:29 +0300

    token

--- cway/stock-app/api/src/models/token.js
+++ cway/stock-app/api/src/models/token.js
@@ -1,3 +1,30 @@
 "use strict";
 
 const { mongoose } = require("../configs/dbConnection");
+
+const TokenSchema = new mongoose.Schema(
+	{
+		user_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true,
+		},
+		token: {
+			type: String,
+			trim: true,
+			required: true,
+			index: true,
+		},
+	},
+	{ collection: "tokens", timestamps: true }
+);
+
+/* ------------------------------------------------------- */
+// FOR REACT PROJECT:
+TokenSchema.pre("init", function (data) {
+	data.id = data._id;
+	data.createds = data.createdAt.toLocaleDateString("tr-tr");
+});
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Token", TokenSchema);
--- cway/stock-app/api/src/models/user.js
+++ cway/stock-app/api/src/models/user.js
@@ -1,3 +1,89 @@
 "use strict";
 
 const { mongoose } = require("../configs/dbConnection");
+
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		first_name: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		last_name: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		is_active: {
+			type: Boolean,
+			default: true,
+		},
+		is_staff: {
+			type: Boolean,
+			default: false,
+		},
+		is_superadmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ collection: "users", timestamps: true }
+);
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+UserSchema.pre(["save", "updateOne"], function (next) {
+	// get data from "this" when create;
+	// if process is updateOne, data will receive in "this._update"
+	const data = this?._update || this;
+
+	// email@domain.com
+	const isEmailValidated = data.email
+		? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".
+		: true;
+
+	if (isEmailValidated) {
+		if (data?.password) {
+			// pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
+			const isPasswordValidated =
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(
+					data.password
+				);
+
+			if (isPasswordValidated) {
+				this.password = data.password = passwordEncrypt(data.password);
+				this._update = data; // updateOne will wait data from "this._update".
+			} else {
+				next(new Error("Password not validated."));
+			}
+		}
+
+		next(); // Allow to save.
+	} else {
+		next(new Error("Email not validated."));
+	}
+});
+/* ------------------------------------------------------- */
+// FOR REACT PROJECT:
+UserSchema.pre("init", function (data) {
+	data.id = data._id;
+	data.createds = data.createdAt.toLocaleDateString("tr-tr");
+});
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("User", UserSchema);
